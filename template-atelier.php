80c2a664f79439befadc204829f021bc8d895752
MarcSicotte <sicotte13@live.fr>
2022-05-19 14:32:06 -0400

    ef-MAS arrangement css template atelier

--- template-atelier.php
+++ template-atelier.php
@@ -12,38 +12,47 @@
 <main class="site__main-type2">
     <article class="atelier">
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>   
-   <h1><?php the_title(); ?></h1>
+   <h1>Atelier: <?php the_title(); ?></h1>
     <div class="site__main-type2__content">  
             <section class="atelier__desc">
+                <h3>Description de l'atelier</h3>
                 <?php the_field('description'); ?>
             </section>
             <div class="atelier__infos">
                 <p class="atelier__endroit">
-                    <?php the_field('animateur'); ?>
+                    <h5>L'animateur de l'atelier</h5>
+                    <h3><?php the_field('animateur'); ?></h3>
                 </p>
                 <p class="atelier__endroit">
-                    <?php the_field('date_debut'); ?>
+                    <h5>l'atelier sera donné au local:</h5>
+                    <h3><?php the_field('local'); ?><h3>
                 </p>
                 <p class="atelier__endroit">
-                    <?php the_field('date_fin'); ?>
+                    <h5>Date de début:</h5>
+                    <h3><?php the_field('date_debut'); ?></h3>
                 </p>
                 <p class="atelier__endroit">
-                    <?php the_field('jours'); ?>
+                   <h5> Date de fin: </h5>
+                    <h3><?php the_field('date_fin'); ?></h3>
                 </p>
                 <p class="atelier__endroit">
-                    <?php the_field('heure_debut'); ?>
+                   <h5> La formation se donnera:</h5>
+                    <h3><?php the_field('jours'); ?></h3>
                 </p>
                 <p class="atelier__endroit">
-                    <?php the_field('heure_fin'); ?>
+                    <h5>La durée en nombre d'heure: </h5>
+                    <h3><?php the_field('duree'); ?></h3>
                 </p>
                 <p class="atelier__endroit">
-                    <?php the_field('duree'); ?>
+                    <h5>L'horaire: </h5>
+                    De <h3><?php the_field('heure_debut'); ?></h3> à <h3><?php the_field('heure_fin'); ?></h3>
                 </p>
-                <p class="atelier__endroit">
-                    <?php the_field('local'); ?>
-                </p>
+                
+                    
+                
+                
             </div>
-            <h2>Venez en grand nombre!</h2>
+            
 
             
                 
